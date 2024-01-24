8c47125c0a40c46259c0004baf3049fe1c7364f5
MinhLoc2401 Hoang <minhloc2401.hoang@gmail.com>
2024-01-24 08:50:39 +0700

    update category 4:46

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,22 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CategoriesService } from '../services/categories.service';
 import { Category } from '../models/category';
+import { VariableBinding } from '@angular/compiler';
+import { validateEventsArray } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.css']
 })
-export class CategoriesComponent {
+export class CategoriesComponent implements OnInit {
+  categoryArray: Array<any> = [];
+  formCategory!: string;
+  formStatus: string = 'Add'
+  categoryId!: string
+
   constructor(
     private categoryService: CategoriesService
   ){}
+
+  ngOnInit(): void {
+    this.categoryService.loadData().subscribe( val => {
+      this.categoryArray = val
+    })
+  }
 
   onSubmit(formData: any) {
     let categoryData: Category = {
       category: formData.value.category
     }
 
-    this.categoryService.saveData(categoryData)
+    if (this.formStatus == 'Add') {
+      this.categoryService.saveData(categoryData)
+      formData.reset()
+    } else if (this.formStatus == 'Edit') {
+      this.categoryService.updateData(this.categoryId, categoryData)
+      formData.reset()
+      this.formStatus = 'Add'
+    }
+
+  }
+
+  onEdit(id: string, category: any) {
+    this.formCategory = category
+    this.formStatus = 'Edit'
+    this.categoryId = id
   }
 }
--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { ToastrService } from 'ngx-toastr';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,9 +15,26 @@ export class CategoriesService {
 
   saveData(data: any) {
     this.afs.collection('categories').add(data).then(docRef => {
-      console.log(docRef)
       this.toastr.success('Data insert successfully')
     })
     .catch(err => {console.log(err)})
+  }
+
+  loadData() {
+    return this.afs.collection('categories').snapshotChanges().pipe(
+      map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data()
+          const id = a.payload.doc.id
+          return {id, data}
+        })
+      })
+    )
+  }
+
+  updateData(id: string, EditData: any) {
+    this.afs.collection('categories').doc(id).update(EditData).then(docRef => {
+      this.toastr.success('Data update successfully')
+    })
   }
 }
